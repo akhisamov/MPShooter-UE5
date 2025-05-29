39b73b0c13e6fa88e4b5ec3a44e0ad67c95c9a8c
Amil Khisamov <akhisamov@mailbox.org>
2025-05-29 12:55:12 +0300

    Projectile Classes

---
+++ Source/MPShooter/Weapon/ProjectileWeapon.cpp
@@ -0,0 +1,3 @@
+// Copyright (c) 2025 Amil Khisamov
+
+#include "ProjectileWeapon.h"
---
+++ Source/MPShooter/Weapon/ProjectileWeapon.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 Amil Khisamov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "ProjectileWeapon.generated.h"
+
+UCLASS()
+class MPSHOOTER_API AProjectileWeapon : public AWeapon
+{
+	GENERATED_BODY()
+};
